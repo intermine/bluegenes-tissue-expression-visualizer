c46ade460b8adb24e4e479f571f540ae15756ffc
akshat <akshatbhargavadeepa@gmail.com>
2019-05-29 12:30:52 +0530

    added query test for stageExpression and fixed typos in `common/test`

--- tests/common.test.js
+++ tests/common.test.js
@@ -2,7 +2,7 @@ const imjs = require('imjs');
 const queryOrgName = require('../src/common/queryOrganism').default;
 
 describe('queryOrganism', () => {
-	test('should return a promise with resolves with organism name of gene id passed', () => {
+	test('should return a promise that resolves with organism name of gene id passed', () => {
 		// some fake gene id and test service url
 		const mockData = {
 			geneId: '1000005',
@@ -16,7 +16,7 @@ describe('queryOrganism', () => {
 	test('should throw error if no organism is found for the gene id passed', () => {
 		// some fake gene id and test service url
 		const mockData = {
-			geneId: 'some_wrong_gene_id',
+			geneId: '1000000',
 			serviceUrl: 'http://www.flymine.org/flymine/service'
 		};
 		const queryRes = queryOrgName(mockData.geneId, mockData.serviceUrl, imjs);
---
+++ tests/stageExpression/query.test.js
@@ -0,0 +1,30 @@
+const imjs = require('imjs');
+const queryStageExpData = require('../../src/stageExpression').queryData;
+
+describe('Expression By Stage Module', () => {
+	const mockData = {
+		geneId: '1000005',
+		orgName: 'Drosophila melanogaster',
+		serviceUrl: 'http://www.flymine.org/flymine/service'
+	};
+	test('should return a promise that resolves with correct result json', () => {
+		const queryRes = queryStageExpData(
+			mockData.geneId,
+			mockData.orgName,
+			mockData.serviceUrl,
+			imjs
+		);
+
+		return queryRes.then(res => {
+			expect(res).toHaveProperty('rnaSeqResults');
+			expect(res.rnaSeqResults).toBeInstanceOf(Array);
+
+			const firstVal = res.rnaSeqResults[0];
+			expect(firstVal).toHaveProperty('class');
+			expect(firstVal).toHaveProperty('expressionLevel');
+			expect(firstVal).toHaveProperty('expressionScore');
+			expect(firstVal).toHaveProperty('objectId');
+			expect(firstVal).toHaveProperty('stage');
+		});
+	});
+});
