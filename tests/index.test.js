da80cf6e15f9a48a6c309675413206aeed1dd292
akshat <akshatbhargavadeepa@gmail.com>
2019-05-29 13:41:50 +0530

    wrote test for RootContainer to render "something" in the `elem` passed

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const RootContainer = require('../src/RootContainer').default;
 const main = require('../src').main;
 
 // Example
@@ -7,5 +10,15 @@ describe('main', () => {
 			// testing with all falsy values
 			main('', 0, null, undefined, []);
 		}).toThrowError('Call main with correct signature');
+	});
+
+	test('should render something (atleast a div) into the `elem` passed', () => {
+		const el = document.createElement('div');
+		ReactDOM.render(
+			<RootContainer entity={{}} serviceUrl="sdgjknsdg" testing={true} />,
+			el
+		);
+		expect(el.innerHTML).toContain('div');
+		expect(el.innerHTML).not.toEqual('');
 	});
 });
